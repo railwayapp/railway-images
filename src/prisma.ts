6bf9d58811d8722561692a209da77db42f3dfc06
Jake Runzer <jakerunzer@gmail.com>
2020-08-07 15:23:57 +0100

    print cwd

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import pg from "railway/pg";
 
+console.log("PWD", process.cwd());
 console.log("CONFIG", pg.config);
 
 export const prisma = new PrismaClient({
