77139cc5138656bf20652effc800625c170021f6
Jake Runzer <jakerunzer@gmail.com>
2020-08-07 14:24:41 +0100

    prisma images and ui

---
+++ pages/api/images.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "GET") {
+    const images = await prisma.image.findMany();
+    res.status(200).json(images);
+  } else if (req.method === "POST") {
+    const { src } = req.body;
+
+    if (src == null || src.trim() === "") {
+      return res.status(400).send({ error: "src required in body" });
+    }
+
+    const image = await prisma.image.create({
+      data: { src },
+    });
+
+    res.status(200).json(image);
+  }
+};
+
+export default handler;
---
+++ pages/index.tsx
@@ -0,0 +1,101 @@
+import Head from "next/head";
+import { NextPage, GetServerSideProps } from "next";
+import styles from "../styles/Home.module.css";
+import { useState, FormEvent } from "react";
+import useSWR from "swr";
+import { PrismaClient, Image } from "@prisma/client";
+
+const fetcher = (url: string) => fetch(url).then(res => res.json());
+
+const Home: NextPage<{ images: Image[] }> = props => {
+  const [src, setSrc] = useState("");
+  const { data: images, mutate, revalidate } = useSWR<Image[]>(
+    "/api/images",
+    fetcher,
+    { initialData: props.images },
+  );
+
+  const onSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    if (src.trim() === "") {
+      return;
+    }
+
+    setSrc("");
+
+    mutate(
+      [...images, { createdAt: new Date(), src, id: Math.random().toString() }],
+      false,
+    );
+    await fetch("/api/images", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ src }),
+    });
+
+    revalidate();
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Railway on Vercel</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Images</h1>
+
+        <p className={styles.description}>
+          Built with <a href="https://railway.app">Railway</a>
+        </p>
+
+        <form className={styles.form} onSubmit={onSubmit}>
+          <input
+            name="image"
+            value={src}
+            placeholder="Enter an image URL"
+            onChange={e => setSrc(e.target.value)}
+            className={styles.input}
+          />
+          <button type="submit" className={styles.button}>
+            Enter
+          </button>
+        </form>
+
+        <div className={styles.grid}>
+          {images != null &&
+            images
+              .sort(
+                (a, b) =>
+                  new Date(b.createdAt).getTime() -
+                  new Date(a.createdAt).getTime(),
+              )
+              .map(image => (
+                <a href={image.src} className={styles.card} key={image.src}>
+                  <img src={image.src} className={styles.image} />
+                </a>
+              ))}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const prisma = new PrismaClient();
+  const images = await prisma.image.findMany();
+  return {
+    props: {
+      images: images.map(image => ({
+        ...image,
+        createdAt: image.createdAt.toString(),
+      })),
+    },
+  };
+};
