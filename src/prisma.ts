ff59ffcd8faddd7f48b05f6c56030ee5b2d77b98
Jake Runzer <jakerunzer@gmail.com>
2020-08-07 15:29:48 +0100

    print dir tree

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,8 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import pg from "railway/pg";
+import dirTree from "directory-tree";
 
 console.log("PWD", process.cwd());
 console.log("CONFIG", pg.config);
+
+const tree = dirTree("/some/path");
+console.log(tree);
 
 export const prisma = new PrismaClient({
   datasources: {
