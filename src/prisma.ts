3386a43c8a6583df08845328fc04b3c91dff3d99
Jake Runzer <jakerunzer@gmail.com>
2020-08-07 15:20:26 +0100

    log pg config

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import pg from "railway/pg";
 
+console.log("CONFIG", pg.config);
+
 export const prisma = new PrismaClient({
   datasources: {
     db: {
