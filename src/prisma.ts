116d42c16dc765011b89e156a20d9ddff196028f
Jake Runzer <jakerunzer@gmail.com>
2020-08-07 15:10:42 +0100

    update railway

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,13 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 import pg from "railway/pg";
 
-const { user, password, host, port, database } = pg.config;
-const url = `postgresql://${user}:${password}@${host}:${port}/${database}`;
-
 export const prisma = new PrismaClient({
   datasources: {
     db: {
-      url,
+      url: pg.config.url,
     },
   },
 });
