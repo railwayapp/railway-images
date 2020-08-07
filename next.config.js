e1b7327594c63e8825820ebfcd315f8908191cac
Jake Runzer <jakerunzer@gmail.com>
2020-08-07 22:59:45 +0100

    webpack ignore pg-native

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+const webpack = require("webpack");
+
 module.exports = {
   target: "serverless",
+  webpack: config => {
+    config.plugins.push(new webpack.IgnorePlugin(/^pg-native$/));
+
+    return config;
+  },
 };
