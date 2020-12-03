91802678c4a16f79a3e6a438c9e1f60b56796098
Jake Runzer <jakerunzer@gmail.com>
2020-12-02 23:07:44 -0700

    update to latest railway and fix prisma

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,10 +1,3 @@
 import { PrismaClient } from "@prisma/client";
-import pg from "railway/pg";
 
-export const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: pg.config.url,
-    },
-  },
-});
+export const prisma = new PrismaClient();
